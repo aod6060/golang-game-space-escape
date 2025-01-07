1eba993137140b23e69a1980db06fa75e0c05c37
Frederick R. Cook <frcook.6060@gmail.com>
2025-01-07 15:04:01 -0600

    Got the quad to move on screen and I'm impressed with how fast golang is.

--- game/game.go
+++ game/game.go
@@ -6,6 +6,7 @@ import sdl "github.com/veandco/go-sdl2/sdl"
 //import "github.com/go-gl/gl/v4.1-compatibility/gl"
 import "github.com/aod6060/golang-game-space-escape/engine/vmath"
 import "github.com/aod6060/golang-game-space-escape/engine/app"
+import "github.com/aod6060/golang-game-space-escape/engine/input"
 import "github.com/aod6060/golang-game-space-escape/engine/render"
 
 //import "github.com/aod6060/golang-game-space-escape/engine/input"
@@ -13,7 +14,12 @@ import "github.com/aod6060/golang-game-space-escape/engine/render"
 
 var count int32 = 0
 
+
+var pos vmath.Vec3
+var speed float32 = 128.0
+
 func Init() {
+	pos = vmath.Vec3Create(32.0, 32.0, 0.0)
 
 }
 
@@ -21,6 +27,15 @@ func HandleEvent(e sdl.Event) {
 }
 
 func Update(delta float32) {
+
+	var dir vmath.Vec3 = vmath.Vec3Create(
+		input.GetKeyPressedAxis(input.KEYS_LEFT, input.KEYS_RIGHT),
+		input.GetKeyPressedAxis(input.KEYS_UP, input.KEYS_DOWN),
+		0.0)
+
+	var vel = vmath.Vec3Mul(&dir, (delta * speed))
+
+	pos = vmath.Vec3Add(&pos, &vel)
 }
 
 func Render() {
@@ -45,7 +60,7 @@ func Render() {
 	var v vmath.Vec3
 
 	v = vmath.Vec3Create(32.0, 32.0, 0.0)
-	t = vmath.TransformTranslate(&v)
+	t = vmath.TransformTranslate(&pos)
 	s = vmath.TransformScale(&v)
 
 	model = vmath.Mat4MulMatrix(&s, &t)
